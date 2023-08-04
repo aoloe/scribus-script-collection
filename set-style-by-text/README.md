9582fbd312e42956f4454fc47da5011926e5dde7
ale rimoldi <ale@graphicslab.org>
2023-08-04 20:27:24 +0200

    search for text and apply a char style

---
+++ set-style-by-text/README.md
@@ -0,0 +1,11 @@
+# Apply a character style to the matching text
+
+Search the whole document for a specific text and apply a character style to it.
+
+At the start of the script, you need to set the text to be searched (an horizontal ellipsis … in the script) and the name of the charcter style (`fancy style` in the script)
+
+```py
+search_text = '…'
+character_style = 'fancy style'
+```
+
---
+++ set-style-by-text/set-style-by-text.py
@@ -0,0 +1,48 @@
+# go through all text items in the document, search for a text snippet and apply a character style
+#
+# © mit, ale rimoldi, 2023
+
+import re
+
+search_text = '…'
+character_style = 'fancy style'
+
+try:
+    import scribus
+except ImportError:
+    print('This script must be run from inside Scribus')
+    sys.exit()
+
+def main():
+    if not scribus.haveDoc():
+        return
+
+    if character_style not in scribus.getCharStyles():
+        scribus.createCharStyle(character_style)
+
+    current_item = scribus.getSelectedObject()
+
+    # scribus.setRedraw(False)
+    for page in range(1, scribus.pageCount() + 1):
+        scribus.gotoPage(page)
+
+        page_text_frames = [item[0] for item in scribus.getPageItems()
+            if item[1] == 4 and scribus.getPrevLinkedFrame(item[0]) is None]
+
+        for item in page_text_frames:
+            scribus.deselectAll()
+            scribus.selectObject(item)
+            story_text = scribus.getAllText()
+            for m in re.finditer(search_text, story_text):
+                print(m.start())
+                print(m.end())
+                scribus.selectText(m.start(), m.end() - m.start())
+                scribus.setCharacterStyle(character_style)
+
+    scribus.deselectAll()
+    if current_item != '':
+        scribus.selectObject(current_item)
+    # scribus.setRedraw(True)
+
+if __name__ == "__main__":
+    main()
