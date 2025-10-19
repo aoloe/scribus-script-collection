abf8af3db77ab0a190bb7368c060fa1da792afcb
ale rimoldi <ale@graphicslab.org>
2025-10-19 09:36:54 +0200

    doc

--- items-format/set-text-distance-in-calendar.py
+++ items-format/set-text-distance-in-calendar.py
@@ -1,3 +1,11 @@
+"""
+Apply the distances defined in TEXT_DISTANCE_LTRB (left, top, right, bottom...)
+to each text frame in the document that only contains digits (in your case the frames with the dates)
+
+There is no way to undo the changes, so if you're not happy with the result,
+you can run again the same script with different values or close the document without saving it.
+"""
+
 old_unit = scribus.getUnit()
 scribus.setUnit(scribus.UNIT_MM)
 
